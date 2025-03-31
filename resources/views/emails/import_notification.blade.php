e31fac2dbcdcb7777f3c225cd83b0ce8f39832f5
uesleirodrigues <57241287+uesleirodrigues@users.noreply.github.com>
2025-03-31 10:47:08 -0300

    Refatoração

--- resources/views/emails/import_notification.blade.php
+++ resources/views/emails/import_notification.blade.php
@@ -6,8 +6,7 @@
 <body>
     <h1>Importação de Colaboradores Concluída</h1>
     <p>Olá,</p>
-    <p>A importação do arquivo <strong>{{ $fileName }}</strong> foi concluída com sucesso.</p>
-    <p>Os novos colaboradores já estão disponíveis. Você pode conferir a lista atualizada em nosso sistema.</p>
+    <p>A importação do arquivo <strong>{{ $fileName }}</strong> foi concluída. Processamento realizado com sucesso.</p>
     <p>Obrigado,</p>
     <p>{{ config('app.name') }}</p>
 </body>
